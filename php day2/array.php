f8aebb4543c4d78acf37f546436f42546f6fc12b
Mahjabeen Akhtar <themahibabe@hotmail.com>
2024-11-06 09:57:13 +0500

    arrays

---
+++ php day2/array.php
@@ -0,0 +1,48 @@
+<?php 
+// $colors= array("red","yellow","pink","orange","white");
+
+// echo $colors[0]; //indexed array
+
+
+// first way to initialize array
+// $colors= array("red",22,"pink",12.50,"white");
+
+// echo $colors[0]."<br>";
+// echo $colors[1]."<br>";
+// echo $colors[2]."<br>";
+// echo $colors[3]."<br>";
+
+// // second way to initialize array
+// $colors= array["red",22,"pink",12.50,"white"];
+
+// for decorating array 
+// echo "<pre>";
+// print_r($colors); // for printing arrays
+// echo "</pre>";
+
+
+// third way to initialize array
+// $colors[0] = "red";
+// $colors[0] = "pink";
+// $colors[0] = "yellow";
+// $colors[0] = "purple";
+// so we have to write it again and again it increases the code length
+// so other than this we can use loops
+
+// $colors= array("red",22,"pink",12.50,"white");
+
+// for ($i = 0; $i <= 4; $i++) {
+// echo $colors[$i]."<br>";
+
+// }
+
+
+// to print it with unordered or ordered list
+// $colors= ["red",22,"pink",12.50,"white" ];
+
+// echo"<ul";
+// for ($i = 0; $i <= 4; $i++) {
+// echo "<li> $colors[$i] </li>";
+// }
+// echo "</ul>";
+?>
