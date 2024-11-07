101da6b54e64c79f6bc45b05f56076ad821f6e53
Mahjabeen Akhtar <themahibabe@hotmail.com>
2024-11-07 10:10:36 +0500

    explode and implode function of an array

--- php day3/arrayMethods.php
+++ php day3/arrayMethods.php
@@ -1,4 +1,25 @@
 <?php
 
+// function of array 
+// array range(start,end,step)
+// for suppose we give it 1 and 5 and we want range in between of it and we will use range function 
+// step by default value is 1, its optional to give it
+// it also works on characters 
+
+// $newarray = range(1,10,2);
+
+// print_r($newarray); 
+
+// you can also use it in foreach loop
+// foreach(range('a','k') as $letter) {
+//     echo''.$letter.'';
+// }
+
+// explode and implode function of an array
+// explode(seperator,string,limit) if we want to convert a string value in an array
+// limit is optional parameter it works as whatever we give value is limit it will  make that many indexes of array
+
+// implode(seperator,array) if you want to convert an array into a string
+// there is one function similar to implode that is join, no difference in between both
 
 ?>
